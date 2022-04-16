2e8a8ab42854aa5a299a054a369bc84ba5ff3705
Erind <erind@Office-iMac.local>
2022-04-17 10:52:26 +1200

    server.js changes

--- server.js
+++ server.js
@@ -7,5 +7,8 @@ app.get('/', (req, res) => {
     res.send('API running');
 })
 
+// AFTER NPM RUN SERVER (NODEMON SERVER):
+// GO TO POSTMAN, RUN GET AT http://localhost:5000 AND YOU'LL SEE THE "API RUNNING"
+// maybe you forgot why? because app.get() it's creating a handler
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on ${PORT}`))
