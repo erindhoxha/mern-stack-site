7a2e72fb1b07a00afe3ff46068cf8350c3dd6ab8
Erind <erind@Office-iMac.local>
2022-04-17 14:32:32 +1200

    server js added express and express validator

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,11 +1,26 @@
 const express = require('express');
+
 const router = express.Router();
 
-// @route       GET api/users
-// @desc        Test route
+const { body, validationResult } = require('express-validator');
+
+// @route       POST api/users
+// @desc        Register User
 // @access      Public
-router.get('/', (req, res) => {
-    res.send('User route...!');
-})
+router.post('/',
+    // username must be an email
+    body('name').not().isEmpty().trim().escape(),
+    // password must be at least 5 chars long
+    (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+        res.send('User route');
+        console.log(req.body);
+        console.log(req.body.name);
+    });
+
+
 
 module.exports = router;
