2227ab700855e2f9737649177dd9f37fbc649e1e
Erind <erind@Office-iMac.local>
2022-04-18 17:05:19 +1200

    auth users changes

--- routes/api/users.js
+++ routes/api/users.js
@@ -25,15 +25,15 @@ router.post('/',
 
         const { name, email, password } = req.body;
 
-        // Return jsonwebtoken
         try {
 
+            // Check if user exists
             let user = await User.findOne({ email });
-
             if (user) {
                 return res.status(400).json({ errors: [{ msg: "User already exists" }] })
             }
 
+            // Create gravatar
             const avatar = gravatar.url(email, {
                 s: "200",
                 r: "pg",
@@ -47,6 +47,7 @@ router.post('/',
                 password
             });
 
+            // Hash the password of New User
             const salt = await bcrypt.genSalt(10);
             user.password = await bcrypt.hash(password, salt);
 
@@ -62,7 +63,8 @@ router.post('/',
                 (err, token) => {
                     if (err) throw err;
                     res.json({ token });
-                });
+                }
+            );
 
         } catch (err) {
             res.status(500).send('Server error');
