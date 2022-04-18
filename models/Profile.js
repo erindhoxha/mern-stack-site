b4a848e24596aaa71742aa85da596ff0bbbb61a7
Erind <erind@Office-iMac.local>
2022-04-18 18:20:39 +1200

    added profile schema

---
+++ models/Profile.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const ProfileSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    avatar: {
+        type: String,
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = Profile = mongoose.model('user', ProfileSchema);
