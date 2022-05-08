e575bd183fb7e0d958e8f4c08981bb03411588f2
Erind <erind_cbh@hotmail.com>
2022-05-08 17:59:46 +1200

    making posts api

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
+const User = require('../../models/User');
+const { json } = require('express/lib/response');
+const { body, validationResult } = require('express-validator');
 
 // @route       GET api/posts
 // @desc        Test route
--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,8 +1,8 @@
 const express = require('express');
+const router = express.Router();
 const auth = require('../../middleware/auth');
 const Profile = require('../../models/Profile');
 const User = require('../../models/User');
-const router = express.Router();
 const { body, validationResult } = require('express-validator');
 const { json } = require('express/lib/response');
 
