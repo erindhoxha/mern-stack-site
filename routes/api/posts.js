563c123e43e929c899d496b54232014f27a2136f
Erind <erind_cbh@hotmail.com>
2022-05-29 17:22:28 +1200

    post not found msg

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -61,10 +61,15 @@ router.get('/', async (req, res) => {
 router.get('/:id', auth, async (req, res) => {
     try {
         const post = await Post.findById(req.params.id);
-        console.log(post);
-        res.send(post);
+        if (!post) {
+            return res.status(404).json({msg: "Post not found"});
+        }
+        res.json(post);
     } catch(err) {
         console.log(err.message);
+        if (err.kind === 'ObjectId') {
+            return res.status(404).json({msg: "Post not found"});
+        }
         res.status(500).send("Server error");
     }
 })
