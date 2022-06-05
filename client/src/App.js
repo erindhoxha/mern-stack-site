47a7834e833b3fed4fa2d804dbadee30b6bf3b35
Erind <erind_cbh@hotmail.com>
2022-06-05 17:04:46 +1200

    adding the index in the routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,18 @@
 import './App.css';
 import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Route, Routes, Switch } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 
 const App = () => (
+  <Router>
     <Fragment>
       <Navbar />
-      <Landing />
+      <Routes>
+        <Route index path="/" element={<Landing />} />
+      </Routes>
     </Fragment>
+  </Router>
 );
 
 export default App;
