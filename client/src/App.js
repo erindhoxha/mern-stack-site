b51f39a83b22f72a0ad4176e50335c8ed68e4f71
Erind <erind_cbh@hotmail.com>
2022-06-06 18:07:12 +1200

    adding redux

--- client/src/App.js
+++ client/src/App.js
@@ -5,22 +5,26 @@ import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
+import { Provider } from "react-redux";
+import store from "./store";
 
 const App = () => (
-  <Router>
-    <Fragment>
-      <Navbar />
-      <Routes>
-        <Route index path="/" element={<Landing />} />
-      </Routes>
-      <section className="container">
+  <Provider store={store}>
+    <Router>
+      <Fragment>
+        <Navbar />
         <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
+          <Route index path="/" element={<Landing />} />
         </Routes>
-        </section>
-    </Fragment>
-  </Router>
+        <section className="container">
+          <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+          </Routes>
+          </section>
+      </Fragment>
+    </Router>
+  </Provider>
 );
 
 export default App;
--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,9 +1,72 @@
-import React from 'react'
+import React, { Fragment, useEffect, useState } from 'react';
+import axios from 'axios';
+import { Link } from "react-router-dom";
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
+    const [formData, setFormData] = useState({
+        email: '',
+        password: '',
+    });
+
+    const { email, password } = formData;
+
+    const onChange = e => setFormData({ ...formData, [e.target.name]: e.target.value })
+
+    const onSubmit = async e => {
+        e.preventDefault();
+          // It's passing the data
+          const newUser = {
+              email,
+              password,
+          }
+          try {
+              const config = {
+                  headers: {
+                      'Content-Type': 'application/json'
+                  }
+              }
+  
+              const body = JSON.stringify(newUser);
+  
+              const res = await axios.post('/api/users', body, config);
+  
+              console.log(res.data);
+  
+          } catch (err) {
+              console.error(err.response.data);
+          }
+    }
+
+    return (
+        <Fragment>
+            <h1 className="large text-primary">Sign in</h1>
+            <p className="lead"><i className="fas fa-user"></i> Log in to your account</p>
+            <form className="form" onSubmit={e => onSubmit(e)}>
+                <div className="form-group">
+                    <input 
+                        type="email" 
+                        placeholder="Email Address" 
+                        name="email"
+                        value={email} 
+                        onChange={e => onChange(e)} />
+                </div>
+                <div className="form-group">
+                <input
+                    type="password"
+                    placeholder="Password"
+                    name="password"
+                    minLength="6"
+                    value={password} 
+                    onChange={e => onChange(e)} 
+                />
+                </div>
+                <input type="submit" className="btn btn-primary" value="Login" />
+            </form>
+            <p className="my-1">
+                Don't have an account? <Link to="/register">Register</Link>
+            </p>
+        </Fragment>
+    )
 }
 
-export default Login
+export default Login;
